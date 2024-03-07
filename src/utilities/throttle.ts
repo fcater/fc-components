213fddefb862c2393a169df33a8ec25aa1294f1b
Fcater <fc1303140304@gmail.com>
2024-03-07 17:59:12 +0800

    Fix typescript warning

--- src/utilities/throttle.ts
+++ src/utilities/throttle.ts
@@ -1,14 +1,12 @@
 let timer: NodeJS.Timeout | null = null;
 
 const throttle = (fn: Function, wait = 0, immediately = true) => {
-  return function () {
+  return function (this: Function) {
     const args = arguments;
-
     const callback = () => fn.apply(this, Array.prototype.slice.call(args, 0));
 
     if (!timer) {
       if (immediately) callback();
-
       timer = setTimeout(() => {
         if (!immediately) callback();
         timer = null;
